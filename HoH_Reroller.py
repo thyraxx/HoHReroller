3593630629ed45e13952038e42bc5a2a9b1aceb3
Thyraxx <Taz3008@msn.com>
2019-05-15 00:39:48 +0200

    Cleaned a bit of code and added item images

--- HoH_Reroller.py
+++ HoH_Reroller.py
@@ -1,4 +1,5 @@
 import pyautogui
+import sys
 
 rerollImage = pyautogui.locateOnScreen('reroll_hundred.png', region=(840, 733, 230, 30));
 rerollImageLight = pyautogui.locateOnScreen('reroll_hundred_light.png', region=(840, 733, 230, 30));
@@ -11,27 +12,30 @@ def click():
 	pyautogui.mouseDown(); 
 	pyautogui.mouseUp();
 
+def clickOnItem(itemImage):
+	print("Item Found!");
+	itemImageX = itemImage.left;
+	itemImageY = itemImage.top;
+	pyautogui.moveTo(itemImageX+50, itemImageY);
+	click();
+
 while True:
 	count += 1
-	itemImage = pyautogui.locateOnScreen('oldmap.png', region=(687, 466, 45, 290));
-	#itemImage2 = pyautogui.locateOnScreen('heartseeker.png', region=(687, 466, 45, 290));
+	# itemImage = pyautogui.locateOnScreen('oldmap.png', region=(687, 466, 45, 290));
+	itemImage = pyautogui.locateOnScreen('shaftlock_pick.png', region=(687, 466, 45, 290));
+	itemImage2 = pyautogui.locateOnScreen('markham_stone.png', region=(687, 466, 45, 290));
+	# itemImage2 = pyautogui.locateOnScreen('heartseeker.png', region=(687, 466, 45, 290));
 	if str(itemImage) != 'None':
-		print("Item Found!");
-		itemImageX = itemImage.left;
-		itemImageY = itemImage.top;
-		pyautogui.moveTo(itemImageX+50, itemImageY);
-		click();
+		clickOnItem(itemImage);
+		break;
+	elif str(itemImage2) != 'None':
+		clickOnItem(itemImage2)
 		break;
 	else:
 		pyautogui.moveTo(850, 750);
 		if str(rerollImage) != 'None' or str(rerollImageLight) !='None' or str(rerollImageClicked) !='None':
 			click();
-		# else:
-		# 	if str(rerollImageLight !='None'):
-		# 		click();
-		# 	else:
-		# 		if str(rerollImageClicked) !='None':	
-		# 			click();
+
 
 	if count == 100:
 		break;
